632444d5f9afdcc0c976d7a8d4a5cde3415a08a4
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2026-04-09 21:13:32 +0200

    Implement simple functional CNN for ECG200

--- models/cnn.py
+++ models/cnn.py
@@ -4,12 +4,19 @@ from tensorflow.keras import layers, models
 def build_cnn(input_shape):
     inputs = layers.Input(shape=input_shape)
 
-    # MODIFIER/SUPPRIMER ICI : Ajoutez vos couches cachées selon votre architecture CNN
-    #RAPPEL : LE PROF VEUX DU FONCTIONNEL PAS DU SÉQUENTIEL
-    x = layers.Flatten()(inputs) # Exemple minimal
-    # ------------------------------
+    x = layers.Conv1D(filters=16, kernel_size=5, padding="same", activation="relu")(inputs)
+    x = layers.MaxPooling1D(pool_size=2)(x)
+    x = layers.Conv1D(filters=32, kernel_size=3, padding="same", activation="relu")(x)
+    x = layers.MaxPooling1D(pool_size=2)(x)
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.Dense(32, activation="relu")(x)
 
     outputs = layers.Dense(1, activation='sigmoid')(x)
     model = models.Model(inputs, outputs, name="CNN")
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
     return model
